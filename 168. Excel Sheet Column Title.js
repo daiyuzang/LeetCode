5967c4911f9eb4370af271ab3ef3524356046c5c
fanhua <1824454842@qq.com>
2017-10-13 18:50:12 +0800

    Excel表

--- 168. Excel Sheet Column Title.js
+++ 168. Excel Sheet Column Title.js
@@ -5,10 +5,13 @@
 //https://discuss.leetcode.com/topic/6245/python-solution-with-explanation
 //以上链接为数字与字符的对照表
 var convertToTitle = function(n) {
-    var str = "";
+    /*var str = "";
     while(n>0){
     	str = String.fromCharCode("A".charCodeAt() + parseInt((--n) % 26)) + str;
     	n = parseInt(n / 26);
     }
-    return str;
+    return str;*/
+      if (n <= 0) return "";
+    if (n <= 26) return String.fromCharCode("A".charCodeAt() + n - 1);
+    return convertToTitle(n % 26 ? parseInt(n / 26) : parseInt(n / 26 - 1)) + convertToTitle(n % 26 ? parseInt(n % 26) : 26);
 };
