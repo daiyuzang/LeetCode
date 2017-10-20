268779ccdb1a207818f653572b334e2adfd157d6
fanhua <1824454842@qq.com>
2017-10-20 18:14:04 +0800

    删除链表中与给定值相同的节点

---
+++ 203. Remove Linked List Elements.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    var root = new ListNode();
+    root.next = head;
+    var prev = new ListNode();
+    prev = root;
+    while(prev.next != null){
+    	if (prev.next.val === val) {
+    		prev.next = prev.next.next;
+    	}
+    	else{
+    		prev = prev.next;
+    	}
+    }
+    return root.next;
+};
