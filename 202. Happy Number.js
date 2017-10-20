b7be559a37a927a9e752a0557e82ee11a3901b74
fanhua <1824454842@qq.com>
2017-10-20 18:13:07 +0800

    判断一个数是否具有某种规律

---
+++ 202. Happy Number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    var store = {};
+    while(!store[n] && n != 1){
+    	store[n] = n;
+    	n.toString().split("").forEach(function(v,i){
+    		if (i == 0) n = 0;
+    		n += v * v;
+    	});
+    	n = parseInt(n);
+    }
+    return n === 1;
+};
