4ecdadf58f0888246e3465f0492d602b73ee52c2
daiyuzang <1824454842@qq.com>
2017-11-02 16:02:53 +0800

    给定一个数字，判断数组内相同数字位置差比之小“
    git commit -m 给定一个数字，判断数组内相同数字位置差比之小“

--- 219. Contains Duplicate II.js
+++ 219. Contains Duplicate II.js
@@ -3,20 +3,13 @@
  * @param {number} k
  * @return {boolean}
  */
-var containsNearbyDuplicate = function(nums, k) {
-	if(!nums.length || nums.length === 1) return false;
-    if(k <= 0) return false;
-    var arr = {}, m;
-    for (var i = 0; i < nums.length; i++) {
-    	if (arr[nums[i]] === undefined) {
-    		arr[nums[i]] = i;
-    	}
-    	else{
-    		m = Math.abs(arr[nums[i]] - i);
-    		//console.log(m);
-    		break;
-    	}
+var containsNearbyDuplicate = function (nums, k) {
+    for (var i = 0; i < nums.length - 1; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j] && Math.abs(i - j) <= k) {
+                return true;
+            }
+        }
     }
-    if (m <= k) return true;
-    else   return false;
+    return false;
 };
