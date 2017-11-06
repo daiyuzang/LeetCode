e6695cbc77ab7834498cd283a3a4de2945ad7fdc
daiyuzang <1824454842@qq.com>
2017-11-06 12:32:27 +0800

    判断两个字符串是否为交换位置的相同字符

---
+++ 242. Valid Anagram.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+    var a = {
+        "a": 23,
+        "b": 33
+    };
+    var b = {
+        "b": 33,
+        "a": 23
+    };
+    console.log(a === b);
+};
