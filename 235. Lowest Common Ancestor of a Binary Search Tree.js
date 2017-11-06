0775a23ea8f11588642b1ef9ee0795c3eeffc0dc
daiyuzang <1824454842@qq.com>
2017-11-06 08:55:00 +0800

    二叉树两节点最小父节点

--- 235. Lowest Common Ancestor of a Binary Search Tree.js
+++ 235. Lowest Common Ancestor of a Binary Search Tree.js
@@ -12,8 +12,13 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function (root, p, q) {
-    var arrs = [];
-    while (root) {
-        arrs.push(root)
-    }
+    //二叉排序树！根节点在排序中间
+    if (!root || !p || !q)
+        return null;
+    if (Math.max(p.val, q.val) < root.val)
+        return lowestCommonAncestor(root.left, p, q);
+    else if (Math.min(p.val, q.val) > root.val)
+        return lowestCommonAncestor(root.right, p, q);
+    else
+        return root;
 };
