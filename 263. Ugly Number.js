687ebe43460f0e14c336130b9ceff7d1d4eb6c85
daiyuzang <1824454842@qq.com>
2017-11-07 08:39:14 +0800

    整除2、3、5的数

--- 263. Ugly Number.js
+++ 263. Ugly Number.js
@@ -3,6 +3,7 @@
  * @return {boolean}
  */
 var isUgly = function (num) {
+    //整除2、3、5的数
     if (num <= 0) return false;
     while (num >= 2 && num % 2 == 0) num /= 2;
     while (num >= 3 && num % 3 == 0) num /= 3;
