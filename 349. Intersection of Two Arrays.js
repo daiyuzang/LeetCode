641bfe5ef948bbf28368eb518c3c5905350b3bb5
daiyuzang <1824454842@qq.com>
2017-11-10 10:22:45 +0800

    找出两数组的交集，并去掉重复数字

---
+++ 349. Intersection of Two Arrays.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) {
+    nums1.sort(function (val1, val2) {
+        return val1 - val2;
+    });
+    nums2.sort(function (val1, val2) {
+        return val1 - val2;
+    });
+    var arr = [];
+    var flag;
+    for (var i = 0, j = 0; i < nums1.length && j < nums2.length;) {
+        if (nums1[i] < nums2[j])
+            i++;
+        else if (nums1[i] > nums2[j])
+            j++;
+        else {
+            if (nums1[i] != flag) {
+                arr.push(nums1[i]);
+                flag = nums1[i];
+            }
+            i++;
+            j++;
+        }
+    }
+    return arr;
+};
