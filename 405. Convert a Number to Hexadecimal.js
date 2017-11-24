4bf88bcfea201131adb257c0448ae83bae7463ee
daiyuzang <1824454842@qq.com>
2017-11-24 10:20:41 +0800

    将数字转为十六进制数字，并包含负数

--- 405. Convert a Number to Hexadecimal.js
+++ 405. Convert a Number to Hexadecimal.js
@@ -3,6 +3,12 @@
  * @return {string}
  */
 var toHex = function (num) {
-    var str = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "a", "b", "c", "d", "e", "f"];
-
+    var str = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+    if (num === 0) return "0";
+    var result = "";
+    while (num != 0) {
+        result = str[num & 0xF] + result;
+        num = num >>> 4;
+    }
+    return result;
 };
